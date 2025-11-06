f54cae8f80e046105dcc6ccb5dff79cb7b3b253b
Alexey <kiselev-academy@yandex.ru>
2025-11-06 11:37:37 +0300

    feat: installation laravel

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-use WhatToWatch\Services\MovieService\MovieRepository;
-
-$client = new \GuzzleHttp\Client();
-$repository = new MovieRepository($client);
-
-$movies = $repository->findMovieById('tt39105467134');
